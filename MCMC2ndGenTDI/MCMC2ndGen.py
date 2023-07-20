d008bab72a269229fb04852951f57d3b7173a7a9
Jessica Page <jp0089@uah.edu>
2023-07-20 14:03:41 -0400

    updating docstring and default settings

--- MCMC2ndGenTDI/MCMC2ndGen.py
+++ MCMC2ndGenTDI/MCMC2ndGen.py
@@ -6,42 +6,42 @@ import matplotlib.pyplot as plt
 
 class BayesTDI():
 
-	"""Class that performs MCMC to estimate the the time-varying spacecraft separations in 
-	the LISA mission. This code generated the results of Page & Littenberg 2023 
+	"""Class that performs MCMC to estimate the the time-varying spacecraft separations in the LISA mission. 
+	
+	This code generated the results of Page & Littenberg 2023 
 	https://arxiv.org/abs/2305.14186. It samples over data simulated by LISA Instrument
 	and LISA Orbits package, but the code takes LISA science, reference and test-mass 
 	measurements in terms of frequency fluctuations.
 
 	Args:
-		data_file: File containing the LISA measurements with column headers 'sij', 'tauij',
+		data_file(str): File containing the LISA measurements with column headers 'sij', 'tauij',
 			'epsij' for measurements received on S/C i from S/C j and well as truth values for S/C 
 			separations Lij as 'mprs_ij'. Data generation code with correct header names is given in 
-			'generate_data_ESA.ipynb' for LISA Instrument simulation used with ESA Orbits in LISA Orbits.
+			'generate_data_ESA.ipynb' for LISA Instrument simulation used with ESA Orbits in LISA Orbits
+			or 'generate_data.ipynb' for Keplerian orbits. (default='LISA_Instrument_Keplerian_orbits_ppr_orbits_2_Hz_86400_sec.dat')
 		cut_off(int): Number of samples to remove at beginning. Make sure it corresponds 
 			to sample you quote in elements_from_Cartesian.ipynb if using ESA (numerical) orbit
-			model for data simulation.
-		f_s(float): Sampling rate in Hz.
-		t_init(float,int): Initial time in seconds of data.
-		f_min(float): Lower bound of LISA band used in log(likelihood) function. 
+			model for data simulation (default=0).
+		f_s(float): Sampling rate in Hz (default=2.0).
+		t_init(float): Initial time in seconds (default=0.0). 
+		f_min(float): Lower bound of LISA band used in log(likelihood) function (default = 5.0e-4). 
 		f_max(float): Upper bound of LISA band used in log(likelihood) function. <0.1 Hz 
-			is recommended. (See Bayesian TDI articles.) 
+			is recommended (default = 0.1).
 		orbit_model(str): Orbit model used in LISA Orbits to simulate the data. Orbit 
-			model determines the parameterization of Lij(t).                                 #Haven't implemented Keplerian yet.
-		orbital_elements_file: File that contains true initial values of the 6 Keplerian 
+			model determines the parameterization of Lij(t) (default = 'keplerian').                                
+		orbital_elements_file(str): File that contains true initial values of the 6 Keplerian 
 			orbital elements for all 3 S/C. Suggested for use in initializing the walkers.
-			This file can be generated from elements_from_Cartesian.ipynb
-		tcb(True, False): whether the data is simulated in the SSB frame or proper psuedo 
-			ranges for delay estimation.
-		number_n(int, even or odd): Filter length of FDI filter.
-		Nens(int):Number of walkers you want to use -1. 
-		Nburnin(int): Number of chains treated as burnin.
-		Nsamples(int): Number of chains.
-		chainfile(str): Name of file storing the samples. A backend .h5 file is also generated 
-
-	
+			This file can be generated from elements_from_Cartesian.ipynb. (default = None)
+		tcb(Bool): whether the data is simulated in the SSB frame or proper psuedo 
+			ranges for delay estimation (default=False).
+		number_n(int): Filter length of FDI filter (default=7).
+		Nens(int):Number of walkers you want to use - 1 (default=37). 
+		Nburnin(int): Number of chains treated as burnin (default=100).
+		Nsamples(int): Number of chains (default=100000).
+		chainfile(str): Name of .dat file storing the samples (default = 'chainfile.dat'). A backend .h5 file is also generated. 
 	"""
-	def __init__(self, data_file, cut_off=0,f_s=4.0,t_init= 13100.0,f_min= 5.0e-4,f_max = 0.1,orbit_model='esa',orbital_elements_file='elements_from_Cartesian_4_Hz_3600_sec.dat',tcb=True,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000,chainfile='chain.dat'):
-		self.data_file = data_file
+	
+	def __init__(self, data_file='LISA_Instrument_Keplerian_orbits_ppr_orbits_2_Hz_86400_sec.dat', cut_off=0,f_s=2.0,t_init=0.0,f_min= 5.0e-4,f_max = 0.1,orbit_model='keplerian',orbital_elements_file=None,tcb=False,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000) 
 		self.f_s = f_s
 		self.number_n = number_n
 		self.cut_off = cut_off
@@ -56,10 +56,11 @@ class BayesTDI():
 		self.Nburnin = Nburnin
 		self.Nsamples = Nsamples
 		self.chainfile = chainfile
-		
+				
 		import settings
 		settings.init(self.data_file,self.f_s,self.number_n,self.cut_off,self.t_init,self.central_freq,self.f_min,self.f_max,self.tcb)  
-		if self.orbit_model=='esa':
+		
+		if self.orbit_model=='numerical':
 		
 			elements_data = np.genfromtxt(self.orbital_elements_file)
 
@@ -78,9 +79,7 @@ class BayesTDI():
 
 			self.initial_state=self.initial_state.T
 			self.ndims = self.initial_state.shape[1]
-
 			self.initial_state = np.vstack([self.initial_state, initial_state_truth])
-
 			self.Nens+=1
 
 			self.semi_major_0=np.array([elements_data[0],elements_data[1],elements_data[2]])
@@ -119,7 +118,6 @@ class BayesTDI():
 		import time
 		import zeus
 		from multiprocessing import Pool
-
 		from mcmc_functions import target_log_prob_fn
 
 		#........................................................................................
@@ -132,16 +130,11 @@ class BayesTDI():
 
 		cb0 = zeus.callbacks.AutocorrelationCallback(ncheck=100, dact=0.01, nact=10, discard=0.5)
 		cb1 = zeus.callbacks.SaveProgressCallback(self.chainfile, ncheck=100)
-		if __name__ == "__main__": 
-			with Pool() as pool:
-				sampler = zeus.EnsembleSampler(self.Nens, self.ndims, target_log_prob_fn,mu=1e3,pool=pool,args=[einsum_path_to_use])
-				sampler.run_mcmc(self.initial_state, self.Nsamples, callbacks=[cb0,cb1])
-				#sampler.run_mcmc(initial_state_begin_here, Nsamples-len(samples))
+		sampler = zeus.EnsembleSampler(self.Nens, self.ndims, target_log_prob_fn,mu=1e3,args=[einsum_path_to_use])
+		sampler.run_mcmc(self.initial_state, self.Nsamples, callbacks=[cb0,cb1])
 
 	
-
-
-			print("--- %s seconds using multiprocessing---" % (time.time() - start_time))
+		print("--- %s seconds using zeus---" % (time.time() - start_time))
 
 	def run_emcee_Keplerian_mcmc(self,einsum_path_to_use):
 		"""The current sampler for realistic KeplerianLISA orbits. Currently uses the 
@@ -155,8 +148,6 @@ class BayesTDI():
 		"""
 		import time
 		import emcee
-		#import settings
-		#settings.init(self.data_file,self.f_s,self.number_n,self.cut_off,self.t_init,self.central_freq,self.f_min,self.f_max,self.tcb)  
 		from mcmc_functions_Keplerian import target_log_prob_fn
 
 		#........................................................................................
@@ -227,6 +218,8 @@ class BayesTDI():
 				break
 			old_tau = tau
 
+		print("--- %s seconds using emcee---" % (time.time() - start_time))
+
 		n = 100 * np.arange(1, index_here + 1)
 		y = autocorr[:index_here]
 		plt.plot(n, n / 100.0, "--k")
@@ -274,12 +267,13 @@ class BayesTDI():
 
 		return einsum_path_to_use
 
-
-#b1 = BayesTDI('LISA_Instrument_ESA_orbits_tcb_orbits_4_Hz_3600_sec.dat', cut_off=0,f_s=4.0,t_init= 13100.00,f_min= 5.0e-4,f_max = 0.1,orbit_model='esa',orbital_elements_file='elements_from_Cartesian_4_Hz_3600_sec.dat',tcb=True,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000)
-b1 = BayesTDI('LISA_Instrument_ESA_orbits_tcb_orbits_4_Hz_3600_sec.dat', cut_off=0,f_s=4.0,t_init= 13100.00,f_min= 5.0e-4,f_max = 0.1,orbit_model='keplerian',orbital_elements_file='elements_from_Cartesian_4_Hz_3600_sec.dat',tcb=True,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000)
+'''
+b1 = BayesTDI('LISA_Instrument_ESA_orbits_tcb_orbits_4_Hz_3600_sec.dat', cut_off=0,f_s=4.0,t_init= 13100.00,f_min= 5.0e-4,f_max = 0.1,orbit_model='numerical',orbital_elements_file='elements_from_Cartesian_4_Hz_3600_sec.dat',tcb=True,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000)
+#b1 = BayesTDI('LISA_Instrument_ESA_orbits_tcb_orbits_4_Hz_3600_sec.dat', cut_off=0,f_s=4.0,t_init= 13100.00,f_min= 5.0e-4,f_max = 0.1,orbit_model='keplerian',orbital_elements_file='elements_from_Cartesian_4_Hz_3600_sec.dat',tcb=True,number_n=7,Nens = 37,Nburnin = 100,Nsamples = 100000)
 
 einsum_path_to_use = b1.get_einsum_path()
 if b1.orbit_model=='esa':
 	b1.run_zeus_mcmc(einsum_path_to_use)
 else:
 	b1.run_emcee_Keplerian_mcmc(einsum_path_to_use)
+'''
